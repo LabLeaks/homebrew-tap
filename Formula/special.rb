69eeff90b5c539b0991d37a61d468feee305da84
Geordie Kaytes <david.kaytes@gmail.com>
2026-04-16 10:13:56 -0400

    Update Formula/special.rb for v0.4.1

--- Formula/special.rb
+++ Formula/special.rb
@@ -4,7 +4,7 @@
 class Special < Formula
   desc "Repo-native semantic spec tool"
   homepage "https://github.com/LabLeaks/special"
-  version "0.4.0"
+  version "0.4.1"
 
   archive = on_system_conditional(
     macos: on_arch_conditional(
@@ -16,15 +16,15 @@ class Special < Formula
       intel: "special-cli-x86_64-unknown-linux-gnu.tar.xz",
     ),
   )
-  url "https://github.com/LabLeaks/special/releases/download/v0.4.0/#{archive}"
+  url "https://github.com/LabLeaks/special/releases/download/v0.4.1/#{archive}"
   sha256 on_system_conditional(
     macos: on_arch_conditional(
-      arm: "d52645eca9ccaa8a898a39e85893ea20c2ba48980548e123c7acea008b3851c8",
-      intel: "90b64da5e9103c7c76fc2d420f0214df9184c4c8a97ea75c9969c021bab81cf9",
+      arm: "34ce43dc8dfdfa74d5b456c44347c1817bbc3ff5ad3b35fd34e51275dfde91e6",
+      intel: "00b61f5a14d0b17689b7ddbc3afccafd297a77942f426d17e884ef35c2dcb2ff",
     ),
     linux: on_arch_conditional(
-      arm: "dc720938c2feaffee28fccedebe0a6c2e0833d18cdc76f03a2863f91529c0d34",
-      intel: "68aa103b1bcd66e5b16a54d51a4de9d2776233eee16fb59d89aeb442c77e9be1",
+      arm: "8e7bd1dd1b0f4c8f5a795942d131e8141a53fc25ce9cf9a53201fb86a5253584",
+      intel: "18358f2f46d91af9e8e131faf5a7953e4b98d054394bcd139d45448a3b567c88",
     ),
   )
 
